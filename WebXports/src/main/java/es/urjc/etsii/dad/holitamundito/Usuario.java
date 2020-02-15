25d6b8b304051211a9122cdb61fd7c16d94843a2
Jeftkiller <45568177+Jeftkiller@users.noreply.github.com>
2020-02-15 21:23:12 +0100

    Añadido JPA al pom

--- WebXports/src/main/java/es/urjc/etsii/dad/holitamundito/Usuario.java
+++ WebXports/src/main/java/es/urjc/etsii/dad/holitamundito/Usuario.java
@@ -1,5 +1,18 @@
 package es.urjc.etsii.dad.holitamundito;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String correo;
+	
+	
+	
 }
